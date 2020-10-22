a7b9d0434256383cff0e54881e3070868ae111e1
Madeleine Olsson <Madeleine.olsson@live.com>
2020-10-22 12:25:46 +0200

    Initial add project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FianerdTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
